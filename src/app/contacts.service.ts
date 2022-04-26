b3a8ac9272d55890f894ea06c4f0401e4e1f4607
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-04-26 20:37:18 +0300

    fix photo displaying

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { ChatContact } from './model';
 import { CONTACTLIST } from './mock-contacts';
 import { Observable, of } from 'rxjs';
+import { APP_BASE_HREF, Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  constructor(
+    private location: Location,
+  ) { }
 
   getContacts(): Observable<ChatContact[]>{
-    const contacts = of(CONTACTLIST);
+    const contacts = of(CONTACTLIST.map((item: ChatContact): ChatContact => {
+      item.photo = this.location.prepareExternalUrl(item.photo);
+      return item;
+    }));
     return contacts;
   }
 }
